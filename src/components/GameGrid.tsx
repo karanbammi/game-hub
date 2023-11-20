067e7253c44cc3017fe78131e25a0fa025faa00d
Karan Bammi <karanbammi123@gmail.com>
2023-11-20 12:28:27 +0530

    Added Platform Icons

--- src/components/GameGrid.tsx
+++ src/components/GameGrid.tsx
@@ -8,8 +8,8 @@ const GameGrid = () => {
     <>
       <Text>{errors}</Text>
       <SimpleGrid
-        columns={{ sm: 1, md: 2, lg: 3, xl: 5 }}
-        spacing="10px"
+        columns={{ sm: 1, md: 2, lg: 3, xl: 4 }}
+        spacing="10"
         padding={"20px"}
       >
         {games.map((game) => (
